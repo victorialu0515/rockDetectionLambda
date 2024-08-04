a362e32bf028ef4ccf89677a297dd5869e8b0fce
victorialu0515 <victoriafionalu@gmail.com>
2024-08-04 09:28:21 -0400

    try to reduce size

--- build.bash
+++ build.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Building docker image"
-docker build -t 339713152729.dkr.ecr.us-east-2.amazonaws.com/rockdetection -f docker/Dockerfile .
+docker build --squash -t 339713152729.dkr.ecr.us-east-2.amazonaws.com/rockdetection -f docker/Dockerfile .
